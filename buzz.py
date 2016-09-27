9a58d1cdde7cae7455584b8669f7010035809c83
rxseger <rseger@gmx.co.uk>
2016-09-27 05:30:29 +0000

    Add buzz.py test script

---
+++ buzz.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# buzz.py - test magnetic transducer for nightlight
+
+import time
+import math
+import pigpio
+
+pi = pigpio.pi() # sudo pigpiod
+
+# magnetic transducer
+BUZZER_BCM = 19 # board pin #35
+#BUZZER_FREQUENCY = 2000 # Hz, try: 8000, 4000, 2000 (rated), 1000, 800, 500, 200, 100, 80, 50, 10
+BUZZER_FREQUENCY = 10 # Hz
+BUZZER_DUTYCYCLE = 255*0.50
+BUZZER_DURATION = 0.1 # seconds
+
+def set_buzzer(on):
+	if on:
+		pi.set_PWM_frequency(BUZZER_BCM, BUZZER_FREQUENCY)
+		pi.set_PWM_dutycycle(BUZZER_BCM, BUZZER_DUTYCYCLE)
+	else:
+		pi.set_PWM_frequency(BUZZER_BCM, 0)
+		pi.set_PWM_dutycycle(BUZZER_BCM, 0)
+
+on = True
+while True:
+	set_buzzer(on)
+	on = not on
+
+	time.sleep(0.1)
